cc174d6dc39bd4bd0f9fc2180ef0189ee3f5f41e
zfk <zhfk930129@gmail.com>
2012-04-25 17:02:00 +0800

    1

--- HashSet.h
+++ HashSet.h
@@ -44,6 +44,8 @@ class HashSet {
     }
     node **array;
     int length;
+    int siz;
+    int (*key) (const T &x);
 public:
     class ConstIterator {
     public:
@@ -88,17 +90,46 @@ public:
     /**
      * Constructs a empty set with your own default capacity
      */
-    HashSet() {}
+    HashSet() {
+        length = 11;
+        siz = 0;
+        key = H::hashcode;
+        array = new node* [length];
+        for (int i = 0; i < length; ++i) {
+            array[i] = new node();
+        }
+    }
 
     /**
      * Destructor
      */
-    ~HashSet() { }
+    ~HashSet() {
+        node *p, *q;
+        for (int i = 0; i < length; ++i) {
+            p = array[i];
+            while (p != NULL) {
+                q = p->next;
+                delete p;
+                p = q;
+            }
+        }
+        delete []array;
+    }
 
     /**
      * Copy constructor
      */
-    HashSet(const HashSet &c) {}
+    HashSet(const HashSet &c) {
+        /*array = new node* [c.length];
+        for (int i = 0; i < length; ++i) {
+            array[i] = new node();
+            node *p = array[i];
+            node *q = c.a
+            while (p != NULL) {
+
+            }
+        }*/
+    }
 
     /**
      * Assignment operator
@@ -115,31 +146,70 @@ public:
      * Constructs a new, empty set; the backing HashMap instance has the
      * specified capacity
      */
-    HashSet(int capacity) {}
+    HashSet(int capacity) {
+        siz = 0;
+        key = H::hashcode;
+        length = capacity;
+        array = new node* [length];
+        for (int i = 0; i < length; ++i) {
+            array[i] = new node();
+        }
+    }
 
     /**
      * Adds the specified element to this set if it is not already present.
      * Returns false if element is previously in the set.
      * O(1) for average
      */
-    bool add(const T& elem) {}
+    bool add(const T& elem) {
+        int pos = key(elem);
+        node *p = array[pos]->next;
+        while (p != NULL && p->data != elem) p = p->next;
+        if (p == NULL) {
+            p = new node(elem);
+            p->next = array[pos]->next;
+            array[pos]->next = p;
+            ++siz;
+            return true;
+        }
+        return false;
+    }
 
     /**
      * Removes all of the elements from this set.
      */
-    void clear() {}
+    void clear() {
+        node *p, *q;
+        for (int i = 0; i < length; ++i) {
+            p = array[i];
+            while (p != NULL) {
+                q = p->next;
+                delete p;
+                p = q;
+            }
+        }
+        siz = 0;
+    }
 
     /**
      * Returns true if this set contains the specified element.
      * O(1) for average
      */
-    bool contains(const T& elem) const {}
+    bool contains(const T& elem) const {
+        int pos = key(elem) % length;
+        node *p = array[pos];
+        while (p->next != NULL && p->next->data != elem) p = p->next;
+        if (p->next != NULL) return true;
+        return false;
+    }
 
     /**
      * Returns true if this set contains no elements.
      * O(1)
      */
-    bool isEmpty() const {}
+    bool isEmpty() const {
+        return (siz == 0);
+    }
 
     /**
      * Returns an iterator over the elements in this set.
@@ -155,13 +225,27 @@ public:
      * Removes the specified element from this set if it is present.
      * O(1) for average
      */
-    bool remove(const T& elem) {}
+    bool remove(const T& elem) {
+        int pos = key(elem) % length;
+        node *p = array[pos];
+        while (p->next != NULL && p->next->data != elem) p = p->next;
+        if (p->next != NULL) {
+            --siz;
+            node *q = p->next;
+            p->next = q->next;
+            delete q;
+            return true;
+        }
+        return false;
+    }
 
     /**
      * Returns the number of elements in this set (its cardinality).
      * O(1)
      */
-    int size() const {}
+    int size() const {
+        return siz;
+    }
 };
 
 #endif
