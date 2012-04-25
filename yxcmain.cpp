a3d23a4f264d802fdaf1a39b675d7c1c1b502462
Zhipeng Chen <czpcxfzwq@126.com>
2012-04-25 15:20:11 +0800

    1

--- yxcmain.cpp
+++ yxcmain.cpp
@@ -3,9 +3,9 @@
 
 #include "Utility.h"
 //#include "LinkedList.h"
-//#include "ArrayList.h"
+#include "ArrayList.h"
 //#include "TreeSet.h"
-#include "TreeMap.h"
+//#include "TreeMap.h"
 //#include "HashSet.h"
 //#include "HashMap.h"
 
@@ -20,7 +20,7 @@ using namespace std;
     }
     cout << endl;
 }
-
+*/
 void displayArrayList(ArrayList<char> a)
 {
     ArrayList<char>::Iterator iter = a.iterator();
@@ -30,7 +30,7 @@ void displayArrayList(ArrayList<char> a)
     }
     cout << endl;
 }
-
+/*
 void displayTreeSet(TreeSet<int> a)
 {
     TreeSet<int>::Iterator iter = a.iterator();
@@ -51,16 +51,16 @@ void displayHashSet(HashSet<int,Hashint> a)
     cout << endl;
 }*/
 
-void displayTreeMap(TreeMap<int,char> a)
-{
-    TreeMap<int,char>::Iterator iter = a.iterator();
-    while (iter.hasNext())
-    {
-        Entry<int,char> *tmp = &iter.next();
-        cout << "(" << tmp->key << "," << tmp->value << ") ";
-    }
-    cout << endl;
-}
+//void displayTreeMap(TreeMap<int,char> a)
+//{
+//    TreeMap<int,char>::Iterator iter = a.iterator();
+//    while (iter.hasNext())
+//    {
+//        Entry<int,char> *tmp = &iter.next();
+//        cout << "(" << tmp->key << "," << tmp->value << ") ";
+//    }
+//    cout << endl;
+//}
 /*
 void displayHashMap(HashMap<int,char,Hashint> a)
 {
@@ -110,40 +110,40 @@ int main()
 //        link_c = link_a.subList(1,3);/** 取子列a[1..3] */
 //        cout << "subList(1,3): "; displayLinkedList(link_c);
 
-//	cout << "\n--- Testing ArrayList.h ---" << endl;
-//    ArrayList<char> array_a;/** 定义一个char型的Array a */
-//    cout << "\n### Part I : method Add ###" << endl;
-//        for (int i = 1; i <= 10; i++) array_a.add(i+64);/** 正向加入元素A..J */
-//        displayArrayList(array_a);
-//        array_a.clear();/** 清空a */
-//        array_a.add(65);
-//        for (int i = 2; i <= 10; i++) array_a.add(0,i+64);/** 正向加入元素J..A */
-//        displayArrayList(array_a);
-//        array_a.add(3,75);/** 在a[3]加入元素K */
-//        cout << "add(3,K): "; displayArrayList(array_a);
-//        array_a.add(10,76);/** 在a[10]加入元素L */
-//        cout << "add(10,L): "; displayArrayList(array_a);
+	cout << "\n--- Testing ArrayList.h ---" << endl;
+    ArrayList<char> array_a;/** 定义一个char型的Array a */
+    cout << "\n### Part I : method Add ###" << endl;
+        for (int i = 1; i <= 10; i++) array_a.add(i+64);/** 正向加入元素A..J */
+        displayArrayList(array_a);
+        array_a.clear();/** 清空a */
+        array_a.add(65);
+        for (int i = 2; i <= 10; i++) array_a.add(0,i+64);/** 正向加入元素J..A */
+        displayArrayList(array_a);
+        array_a.add(3,75);/** 在a[3]加入元素K */
+        cout << "add(3,K): "; displayArrayList(array_a);
+        array_a.add(10,76);/** 在a[10]加入元素L */
+        cout << "add(10,L): "; displayArrayList(array_a);
 
-//    cout << "\n### Part II : method Get & Contains ###" << endl;
-//        for (int i = 0; i < 12; i++) cout << array_a.get(i) << " ";
-//        cout << endl;
-//        for (int i = 0; i <= 12; i++) cout << array_a.indexOf(i+64) << " ";
-//        cout << endl;/** 返回元素i+64在a中的位置 */
-//        cout << ((array_a.contains('C'))?("contains C"):("not contains C")) << endl;
-//        cout << ((array_a.contains('Z'))?("contains Z"):("not contains Z")) << endl;
+    cout << "\n### Part II : method Get & Contains ###" << endl;
+        for (int i = 0; i < 12; i++) cout << array_a.get(i) << " ";
+        cout << endl;
+        for (int i = 0; i <= 12; i++) cout << array_a.indexOf(i+64) << " ";
+        cout << endl;/** 返回元素i+64在a中的位置 */
+        cout << ((array_a.contains('C'))?("contains C"):("not contains C")) << endl;
+        cout << ((array_a.contains('Z'))?("contains Z"):("not contains Z")) << endl;
 
-//    cout << "\n### Part III : method Remove ###" << endl;
-//        ArrayList<char> array_b(array_a);/** 复制构造 */
-//        array_b.remove(67); array_b.remove(75);/** 删除Array b中的元素C及K*/
-//        cout << "remove(C) & remove(K): "; displayArrayList(array_b);
-//        array_b = array_a;/** ArrayList赋值 */
-//        array_b.removeIndex(5);/** 删除ArrayList b中元素b[5] */
-//        cout << "removeIndex(5): "; displayArrayList(array_b);
-//        array_b = array_a;
-//        array_b.removeIndex(0); array_b.removeIndex(10);/** 删除头元素及尾元素 */
-//        cout << "removeIndex(0) & removeIndex(10): "; displayArrayList(array_b);
-//        array_b.removeRange(0,3);/** 删除[0..3]中的元素 */
-//        cout << "removeRange(0,3): "; displayArrayList(array_b);
+    cout << "\n### Part III : method Remove ###" << endl;
+        ArrayList<char> array_b(array_a);/** 复制构造 */
+        array_b.remove(67); array_b.remove(75);/** 删除Array b中的元素C及K*/
+        cout << "remove(C) & remove(K): "; displayArrayList(array_b);
+        array_b = array_a;/** ArrayList赋值 */
+        array_b.removeIndex(5);/** 删除ArrayList b中元素b[5] */
+        cout << "removeIndex(5): "; displayArrayList(array_b);
+        array_b = array_a;
+        array_b.removeIndex(0); array_b.removeIndex(10);/** 删除头元素及尾元素 */
+        cout << "removeIndex(0) & removeIndex(10): "; displayArrayList(array_b);
+        array_b.removeRange(0,3);/** 删除[0..3]中的元素 */
+        cout << "removeRange(0,3): "; displayArrayList(array_b);
 
 //    cout << "\n### Part IV : method subList ###" << endl;
 //        ArrayList<char> array_c;
@@ -207,14 +207,14 @@ int main()
 //    cout << "\n### Part IV : method explicit HashSet ###" << endl;
 //        HashSet<int,Hashint> hashset_exp_a(treeset_a);
 //        displayHashSet(hashset_exp_a);
-
+/*
 	cout << "\n--- Testing TreeMap.h ---" << endl;
     TreeMap<int,char> treemap_a;
     cout << "\n### Part I : method Add ###" << endl;
        for (int i = 1; i <= 10; i++) treemap_a.put(i,i+64);
         displayTreeMap(treemap_a);
         treemap_a.clear();/** 清空a */
-
+/*
     cout << "\n### Part II : method Get & Contains ###" << endl;
         for (int i = 1; i <= 12; i++) treemap_a.put(i,i+64);
         displayTreeMap(treemap_a);
@@ -226,13 +226,13 @@ int main()
         cout << ((treemap_a.containsKey(15))?("contains Key 15"):("not contains Key 15")) << endl;
         cout << ((treemap_a.containsValue('A'))?("contains Value A"):("not contains Value A")) << endl;
         cout << ((treemap_a.containsValue('Z'))?("contains Value Z"):("not contains Value Z")) << endl;
-
+/*
     cout << "\n### Part III : method Remove ###" << endl;
         TreeMap<int,char> treemap_b(treemap_a);
         treemap_b.remove(3);/** 删除元素Key = 3 */
-        cout << "remove(3) : "; displayTreeMap(treemap_b);
+ /*       cout << "remove(3) : "; displayTreeMap(treemap_b);
         treemap_b.remove(10);/** 删除元素Key = 10 */
-        cout << "remove(10) : "; displayTreeMap(treemap_b);
+/*        cout << "remove(10) : "; displayTreeMap(treemap_b);
         TreeMap<int,char>::Iterator treemap_iter = treemap_b.iterator();
         treemap_iter.next();
         treemap_iter.remove();
